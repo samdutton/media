28c800939c7f3f0d08deaa467a83e896ffb6f33e
Sam Dutton <samdutton@gmail.com>
2013-11-12 19:12:44 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Media APIs for the multi-platform Web
+=====================================
+
+Presentation slides:
+
+* WebRTC: what's new and what's next
+* EME: what is it and how can I use it?
+* Media Source Extensions: for adaptive streaming, time shifting and more
+* Web Audio: sophisticated audio
+* <video> and <audio> update
+* Transcripts, captions and the track element: deep linking and deep search
+* Working with Web Components
+* State of the codecs
+* Best practice for mobile platforms
---
+++ slide_config.js
@@ -0,0 +1,37 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Media APIs for the multi-platform Web',
+    subtitle: 'Chrome Developer Summit',
+    eventTitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    analytics: 'UA-44413410-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    // fonts: [
+    //   'Open Sans:regular,semibold,italic,italicsemibold',
+    //   'Source Code Pro'
+    // ],
+    theme: ['sam'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+    {
+      name: 'Jan Linden',
+      company: 'Senior Product Manager, Google',
+      gplus: 'https://plus.google.com/101097675148849633109/posts'
+    },
+    {
+    name: 'Sam Dutton',
+    company: 'Developer Advocate, Google Chrome',
+    gplus: 'http://plus.samdutton.com',
+    twitter: '@sw12',
+    www: 'http://www.samdutton.com',
+    github: 'http://github.com/samdutton'
+  }
+  ]
+};
+
